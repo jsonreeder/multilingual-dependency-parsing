a2a492923a87b900949d5862b3a5d20adddc0de4
Jason Reeder <info@jasonreeder.com>
2016-08-04 19:44:06 -0700

    Implement left arc

--- code/transition.py
+++ code/transition.py
@@ -19,15 +19,20 @@ class Transition(object):
             :return : A new configuration or -1 if the pre-condition is not satisfied
         """
 
+        # Precondition: Neither the stack nor the buffer are empty
+        if not conf.buffer or not conf.stack:
+            return -1
+
         # Precondition: "wi" not already the dependent of another word
-        # TODO: Add if statement for precondition
+        elif conf.stack[-1] in [dep[2] for dep in conf.arcs]:
+            return -1
+
+        idx_wi = conf.buffer.pop(0)
+        idx_wj = conf.stack[-1]
 
         # Add to arcs
-
-        # Remove from stack
-
-        raise NotImplementedError('Please implement left_arc!')
-        return -1
+        conf.stack.append(idx_wj)
+        conf.arcs.append((idx_wi, relation, idx_wj))
 
     @staticmethod
     def right_arc(conf, relation):
@@ -68,8 +73,8 @@ class Transition(object):
             :return : A new configuration or -1 if the pre-condition is not satisfied
         """
 
-        # Precondition: Neither the stack nor the buffer are empty
-        if not conf.buffer or not conf.stack:
+        # Precondition: The buffer is not empty
+        if not conf.buffer:
             return -1
 
         # Word at the top of the buffer moved to the stack
